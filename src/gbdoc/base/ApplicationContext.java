958fb7e064c04fe7259ef4e55b8783ec4e604a20
coopci <bo.coopci@gmail.com>
2018-11-14 23:44:09 +0800

    add daoManager

--- src/gbdoc/base/ApplicationContext.java
+++ src/gbdoc/base/ApplicationContext.java
@@ -1,6 +1,7 @@
 package gbdoc.base;
 
 import gbdoc.db.DBUtils;
+import gubo.db.DaoManager;
 
 import java.sql.SQLException;
 
@@ -11,4 +12,11 @@ public class ApplicationContext {
 	public DataSource getDataSource() throws SQLException {
 		return DBUtils.getDataSource();
 	}
+
+	private final DaoManager daoManager = new DaoManager();
+
+	public DaoManager getDaoManager() throws SQLException {
+		return daoManager;
+	}
+
 }
