f4c72df64830220a3f0edb7aa46251a3c65ecd99
coopci <bo.coopci@gmail.com>
2018-11-16 18:25:22 +0800

    add @QueryStringField to Standard.java fields.

--- src/gbdoc/db/Standard.java
+++ src/gbdoc/db/Standard.java
@@ -4,25 +4,14 @@ package gbdoc.db;
 import gubo.db.ISimplePoJo;
 import gubo.db.SimplePoJoDAO;
 import gubo.http.querystring.QueryStringField;
-import gubo.jdbc.mapping.InsertStatementGenerator;
-import gubo.jdbc.mapping.ResultSetMapper;
-import gubo.jdbc.mapping.UpdateStatementGenerator;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+
 import java.sql.Timestamp;
-import java.util.LinkedList;
-import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Table;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import java.math.BigDecimal;
-import javax.sql.DataSource;
+import javax.persistence.Table;
 
 @Entity(name = "standard")
 @Table(name = "standard")
@@ -49,9 +38,11 @@ public class Standard implements ISimplePoJo {
 	@Column(name = "updated_by")
 	public Long updated_by = null;
 
+	@QueryStringField()
 	@Column(name = "title")
 	public String title = null;
 
+	@QueryStringField()
 	@Column(name = "template_location")
 	public String template_location = null;
 
