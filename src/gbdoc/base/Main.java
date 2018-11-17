966c2458c9f89af5ccce8140abd2b32d78d2bbc0
jzhao003 <jinhui.zhao@zuora.com>
2018-11-17 21:41:33 +0800

    insert into StandardSection table

--- src/gbdoc/base/Main.java
+++ src/gbdoc/base/Main.java
@@ -3,7 +3,9 @@ package gbdoc.base;
 import org.glassfish.grizzly.http.server.HttpServer;
 
 import gbdoc.db.Standard;
+import gbdoc.db.StandardSection;
 import gbdoc.handlers.CreateRecordsHandler;
+import gbdoc.handlers.GetSectionTreeHandler;
 import gbdoc.handlers.HtmlHandler;
 import gbdoc.handlers.ListAllHandler;
 
@@ -20,6 +22,13 @@ public class Main {
 
 		// curl -XPOST -d 'title=1&template_location=11' 'http://localhost:8777/add'
 		server.getServerConfiguration().addHttpHandler(new CreateRecordsHandler(Standard.class), "/add");
+		
+		// get stanard record by id???
+		// curl -XGET -d  'http://localhost:8777/id'
+		server.getServerConfiguration().addHttpHandler(new GetSectionTreeHandler(appCtx), "/id");
+
+		
+		server.getServerConfiguration().addHttpHandler(new CreateRecordsHandler(StandardSection.class), "/section");
 
 		try {
 
